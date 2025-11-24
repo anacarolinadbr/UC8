ef1b98f9ae48093a61768a61ec69b535de5312fb
Ana Carolina Dambros Bergmann <anacarolinadbr06@gmail.com>
2025-11-23 23:03:28 -0300

    Add files via upload

---
+++ trabalho1/Inimigo.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.trabalho1;
+
+/**
+ *
+ * @author sesia
+ */
+public class Inimigo extends Personagem {
+     
+    public Inimigo(String nome, int vida, int mana, int forca, int agilidade) {
+        super(nome, vida, mana, forca, agilidade);
+    }
+
+    
+    public String usarHabilidade(Personagem inimigo) {
+        return " não possui habilidades";
+    }
+}
---
+++ trabalho1/Trabalho1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.trabalho1;
+
+/**
+ *
+ * @author sesia
+ */
+public class Trabalho1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
